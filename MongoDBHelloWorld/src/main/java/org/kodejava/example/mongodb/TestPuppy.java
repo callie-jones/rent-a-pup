84859f7a1b07aa735e5986beb254cb28d96d8b67
callie-jones <callie.jones@colorado.edu>
2017-03-24 22:34:07 -0600

    added functions for user collection and pet collection

--- MongoDBHelloWorld/src/main/java/org/kodejava/example/mongodb/TestPuppy.java
+++ MongoDBHelloWorld/src/main/java/org/kodejava/example/mongodb/TestPuppy.java
@@ -15,13 +15,13 @@ public class TestPuppy
 
     //creates the database
     //TODO : handle if getClient returns null
-    DB database = getClient().getDB("testPet");
+    static private DB database = getClient().getDB("testPet");
 
     //create collections
-    DBCollection petData = database.getCollection("petData");
-    DBCollection userData = database.getCollection("userData");
+    static private DBCollection petData = database.getCollection("petData");
+    static private DBCollection userData = database.getCollection("userData");
 
-    public MongoClient getClient(){
+    private static MongoClient getClient(){
         try {
             MongoClient client = new MongoClient(new ServerAddress("localhost", 27017));
             return client;
@@ -30,7 +30,7 @@ public class TestPuppy
         return null;
     }
 
-    public Boolean isConnected(){
+    private static Boolean isConnected(){
         try{
             MongoClient client = new MongoClient(new ServerAddress("localhost", 27017));
             DB database = client.getDB("testPet");
@@ -42,17 +42,16 @@ public class TestPuppy
     }
 
     //Admin to add puppy to database
-    public String insertPet(String name, int age, String status, String appt, String info) {
+    public static String addPup(String name, int age, String status, String apt, String info) {
 
         if(isConnected()) {
-
             //BASIC insert data
             //TODO : handle if puppy already in db - dont allow duplicate doc (return "already exists")
             BasicDBObject doc = new BasicDBObject();
             doc.put("name", name);
             doc.put("age", age);
             doc.put("status", status);
-            doc.put("appts", appt); //initially empty
+            doc.put("apts", apt); //initially empty
             doc.put("info", info);
             petData.insert(doc);
 
@@ -62,22 +61,93 @@ public class TestPuppy
     }
 
     //Admin to remove puppy from database
-    public String removePet(){
-        return "";
+    public static String removePup(String name){
+        if(isConnected()){
+            BasicDBObject delDoc = new BasicDBObject();
+            delDoc.put("name", name);
+            //removes doc with specified name
+            petData.remove(delDoc);
 
+            return "Success";
+        }
+        return "Failed";
     }
 
     //Admin to change puppy status
-    public String changeStatus(){
-        return "";
+    public static String changeStatus(String name, String change){
+        if(isConnected()){
+            BasicDBObject oldStat = new BasicDBObject();
+            oldStat.put("name", name);
+
+            BasicDBObject newStat = new BasicDBObject();
+            newStat.put("status", change);
+
+            BasicDBObject updateDoc = new BasicDBObject();
+            updateDoc.put("$set", newStat);
+
+            //updates the status of the doc with the matching pup name
+            petData.update(oldStat, updateDoc);
+            return "Success";
+        }
+        return "Failed";
     }
 
     //User to schedule puppy
+    public static String addApts(String name, String newApt){
+        if(isConnected()){
+            BasicDBObject oldApt = new BasicDBObject();
+            oldApt.put("name", name);
+
+            BasicDBObject addApt = new BasicDBObject();
+            addApt.put("status", newApt);
+
+            BasicDBObject updateDoc = new BasicDBObject();
+            updateDoc.put("$set", addApt);
+
+            //updates the apt of the doc with the matching pup name
+            petData.update(oldApt, updateDoc);
+            return "Success";
+        }
+        return "Failed";
+    }
+
+    //stor user info to userData collection in db
+    public static String addUser(String name, String email, String password){
+        if(isConnected()){
+            BasicDBObject doc = new BasicDBObject();
+            doc.put("name", name);
+            doc.put("password", password);
+            doc.put("email", email);
+            userData.insert(doc);
+            return "Success";
+        }
+        return "Failed";
+    }
 
 
     public static void main( String[] args) {
 
-        //idk what we should do here
+        //just testing rn
+        //add to /etc/mongod.conf
+        //net:
+        //   http:
+        //      enabled: true
+        //      RESTInterfaceEnabled: true
+
+        //if mongod running data displayed at http://localhost:28017/testPet/petData
+
+        //displaying docs where name = callie
+        String complete = addPup("callie", 21, "null", "", "super cool");
+        if(complete == "Success"){
+
+            BasicDBObject searchQuery = new BasicDBObject();
+            searchQuery.put("name", "callie");
+
+            DBCursor cursor = petData.find(searchQuery);
+            while(cursor.hasNext()){
+                System.out.println(cursor.next());
+            }
+        }
 
 }
 
@@ -93,11 +163,11 @@ public class TestPuppy
 //            MongoClient client = new MongoClient(new ServerAddress("localhost", 27017));
 //
 //            //creates the database
-//            DB database = client.getDB("testPet");
+//            DB database = client.getDB("testDB");
 //
 //            //create collection
-//            DBCollection petData = database.getCollection("petData");
-//            DBCollection userData = database.getCollection("userData");
+//            DBCollection petData = database.getCollection("user");
+//            //DBCollection userData = database.getCollection("userData");
 //
 //
 //            //insert data
