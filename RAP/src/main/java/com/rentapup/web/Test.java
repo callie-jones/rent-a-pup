dd40099f93581eedb2e5a8b153b12fb5379d00ac
emstaple <emstaple@gmail.com>
2017-04-15 22:21:39 -0600

    Add User authentication object and test

--- RAP/src/main/java/com/rentapup/web/Test.java
+++ RAP/src/main/java/com/rentapup/web/Test.java
@@ -4,6 +4,9 @@ import com.mongodb.*;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Created by elijahstaple on 4/14/17.
@@ -20,18 +23,35 @@ public class Test {
         }
         DBCollection petData = client.getDB("testPet").getCollection("petData");
         DBCollection renterData = client.getDB("testPet").getCollection("renterData");
+        DBCollection authData = client.getDB("testPet").getCollection("authData");
         Dog testProfile = new Dog("German Shepard", "Don", 14, "Fun doggy and such",
                 System.getProperty("user.dir") + "/assets/images/tiny.png");
         Renter testRenter = new Renter("Jimmy", 32, "Uh, I'm a renter lol",
                 System.getProperty("user.dir") + "/assets/images/tiny.png");
+        String jimmyPassword = "longassjimmy";
+        UserAuthData testAuthData = new UserAuthData("jimmyjim", jimmyPassword, testRenter.getid());
         petData.insert(testProfile);
         renterData.insert(testRenter);
+        authData.insert(testAuthData);
 
         BasicDBObject searchQuery = new BasicDBObject();
         searchQuery.put("name", "Don");
 
         BasicDBObject renterQuery = new BasicDBObject();
         renterQuery.put("name", "Jimmy");
+
+        BasicDBObject authQuery = new BasicDBObject();
+        MessageDigest digest;
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return;
+        }
+        byte[] passwordHash = digest.digest(jimmyPassword.getBytes(StandardCharsets.UTF_8));
+        authQuery.put("passwordHash", passwordHash);
+        authQuery.put("username", "jimmyjim");
+        authQuery.put("userId", testRenter.getid());
 
         DBCursor cursor = petData.find(searchQuery);
         while(cursor.hasNext()){
@@ -57,5 +77,8 @@ public class Test {
             System.out.println(renti.getid());
             System.out.println(renti.getid().getDate());
         }
+
+        if(authData.find(authQuery).count() == 1) System.out.println("Authorization successful!");
+        else System.out.println("Authorization failed :(");
     }
 }
---
+++ RAP/src/main/java/com/rentapup/web/UserAuthData.java
@@ -0,0 +1,54 @@
+package com.rentapup.web;
+
+import com.mongodb.ReflectionDBObject;
+import org.bson.types.ObjectId;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by elijahstaple on 4/15/17.
+ */
+public class UserAuthData extends ReflectionDBObject {
+    private byte[] passwordHash;
+    private String username;
+    private ObjectId userId;
+
+    UserAuthData(String username, String password, ObjectId userId) {
+        MessageDigest digest;
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return;
+        }
+        this.passwordHash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+        this.username = username;
+        this.userId = userId;
+    }
+
+    public byte[] getpasswordHash() {
+        return passwordHash;
+    }
+
+    public void setpasswordHash(byte[] passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getusername() {
+        return username;
+    }
+
+    public void setusername(String username) {
+        this.username = username;
+    }
+
+    public ObjectId getuserId() {
+        return userId;
+    }
+
+    public void setuserId(ObjectId userId) {
+        this.userId = userId;
+    }
+}
