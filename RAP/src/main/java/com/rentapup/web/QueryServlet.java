5f4d3d4fa182602bef1ce5d10ad62c56a23183cb
emstaple <emstaple@gmail.com>
2017-04-16 22:32:26 -0600

    Add Servlet for querying

---
+++ RAP/src/main/java/com/rentapup/web/QueryServlet.java
@@ -0,0 +1,31 @@
+package com.rentapup.web;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rentapup.web.obj.Query;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by elijahstaple on 4/16/17.
+ */
+public class QueryServlet extends HttpServlet{
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream()));
+        String json = "";
+        json = br.readLine();
+
+        ObjectMapper mapper = new ObjectMapper();
+        Query query = mapper.readValue(json, Query.class);
+        response.setContentType("type/string");
+        mapper.writeValue(response.getOutputStream(), "Sucessful AJAX query!!!");
+    }
+}
