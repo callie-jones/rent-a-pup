c3e6ed76dd13212aff2857e447d9570b1ec6ff60
emstaple <emstaple@gmail.com>
2017-04-16 23:42:11 -0600

    Add getters and setters in Query object

--- RAP/src/main/java/com/rentapup/web/obj/Query.java
+++ RAP/src/main/java/com/rentapup/web/obj/Query.java
@@ -8,4 +8,20 @@ import java.util.Map;
 public class Query {
     private QueryType queryType;
     private Map<String, Object> query;
+
+    public QueryType getQueryType() {
+        return queryType;
+    }
+
+    public void setQueryType(QueryType queryType) {
+        this.queryType = queryType;
+    }
+
+    public Map<String, Object> getQuery() {
+        return query;
+    }
+
+    public void setQuery(Map<String, Object> query) {
+        this.query = query;
+    }
 }
