e5b3b5dc74511e5d59fe40e7613c2ddfcbc0a8a6
emstaple <emstaple@gmail.com>
2017-04-20 20:12:12 -0600

    Convert client handling to ServletContext instead of connecting to new client every time

--- RAP/src/main/java/com/rentapup/web/QueryServlet.java
+++ RAP/src/main/java/com/rentapup/web/QueryServlet.java
@@ -26,46 +26,22 @@ import java.security.NoSuchAlgorithmException;
 public class QueryServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    static private DB database = getClient().getDB("rapTest");
-    static private DBCollection authData = database.getCollection("authData");
-    private static MongoClient getClient(){
+    private String testAuth(DBCollection authData, String username, String password){
+        MessageDigest digest;
         try {
-            MongoClient client = new MongoClient(new ServerAddress("localhost", 27017));
-            return client;
-        } catch (UnknownHostException e) { System.out.println(e); }
-        return null;
-    }
-
-    private static Boolean isConnected(){
-        try{
-            MongoClient client = new MongoClient(new ServerAddress("localhost", 27017));
-            DB database = client.getDB("testPet");
-            DBCollection petData = database.getCollection("petData");
-            DBCollection authData = database.getCollection("authData");
-            return true;
-        } catch (UnknownHostException e) { System.out.println(e); }
-        return false;
-    }
-
-    public String testAuth(String username, String password){
-        if(isConnected()) {
-            MessageDigest digest;
-            try {
-                digest = MessageDigest.getInstance("SHA-256");
-            } catch (NoSuchAlgorithmException e) {
-                e.printStackTrace();
-                return null;
-            }
-
-            byte[] passwordHash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
-            DBObject searchquery = QueryBuilder.start("username").is(username)
-                    .and("passwordHash").is(passwordHash).get();
-            DBCursor cursor = authData.find(searchquery, QueryBuilder.start("_id").is(0).and("userId").is(1).get());
-
-            if(cursor.count() == 1){
-                return ((ObjectId) cursor.next().get("userId")).toHexString();
-            }
+            digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
             return null;
+        }
+
+        byte[] passwordHash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+        DBObject searchquery = QueryBuilder.start("username").is(username)
+                .and("passwordHash").is(passwordHash).get();
+        DBCursor cursor = authData.find(searchquery, QueryBuilder.start("_id").is(0).and("userId").is(1).get());
+
+        if(cursor.count() == 1){
+            return ((ObjectId) cursor.next().get("userId")).toHexString();
         }
         return null;
     }
@@ -79,12 +55,16 @@ public class QueryServlet extends HttpServlet {
 
         ObjectMapper mapper = new ObjectMapper();
         Query<String, String> query = mapper.readValue(json, new TypeReference<Query<String, String>>(){});
+
         System.out.print("User : ");
         System.out.println(query.get("user"));
         System.out.print("Password : ");
         System.out.println(query.get("pass"));
-        System.out.println(testAuth(query.get("user"), query.get("pass")));
+
+        DBCollection authCol = ((MongoClient) request.getServletContext().getAttribute("MONGO_CLIENT"))
+                .getDB("rapTest").getCollection("authData");
+        System.out.println(testAuth(authCol, query.get("user"), query.get("pass")));
         response.setContentType("text");
-        mapper.writeValue(response.getOutputStream(), testAuth(query.get("user"), query.get("pass")));
+        mapper.writeValue(response.getOutputStream(), testAuth(authCol, query.get("user"), query.get("pass")));
     }
 }
